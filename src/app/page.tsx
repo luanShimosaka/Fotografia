efe0efc5ce1170aeed115ee5a50f97aa1244edf2
LuanShimosaka <luan.shimosaka@gmail.com>
2025-01-26 19:51:25 -0300

    Remove unused vite.svg file and update layout and page components for improved structure and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "@tailwindcss/postcss";
 
 function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
