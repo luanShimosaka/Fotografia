5d3bf314241d6a41a36925ed340cde6c935dfcd2
LuanShimosaka <luan.shimosaka@gmail.com>
2025-01-26 19:43:35 -0300

    Componente Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import logo from "../assets/logo.png";
 
-export default function Header {
-    return (
-      <div>Header</div>
-    )
+export default function Header() {
+  return (
+    <header>
+      <img src={logo.src} alt="Logo" />
+      <div>
+        <Link href="./"> Início </Link>
+        <Link href="./Sobre"> Sobre Mim </Link>
+        <Link href="./Portfolio"> Portfólio </Link>
+        <Link href="./Orcamento"> Orçamento </Link>
+      </div>
+    </header>
+  );
 }
